9925b30ad63a0fb0904cad1af2bb15b82b21ba17
renatogsouza <renatomacedo1995@gmail.com>
2021-07-03 12:26:06 -0300

    Updates

---
+++ precache-manifest.f0a987ee5de4440915af75a09b6f1eec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e07054a89b70009df98806f8aa7c4061",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0041f74bf52fcec11f1",
+    "url": "/static/css/main.38c99f4d.chunk.css"
+  },
+  {
+    "revision": "2ea3d4698b96f6ddf094",
+    "url": "/static/js/2.fa4e9a33.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.fa4e9a33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0041f74bf52fcec11f1",
+    "url": "/static/js/main.3f299ed1.chunk.js"
+  },
+  {
+    "revision": "84e5482291f212a94bb1",
+    "url": "/static/js/runtime-main.c7e298dc.js"
+  }
+]);
